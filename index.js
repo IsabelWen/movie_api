42d937e64568285dec545e879c1fae474c0b2dc5
Isy <isy1997@freenet.de>
2023-10-27 16:26:08 +0200

    Use express.static to serve documentation.html

--- index.js
+++ index.js
@@ -56,6 +56,8 @@ app.get('/movies', (req, res) => {
     res.json(topMovies);
 });
 
+app.use(express.static('public'));
+
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
 })
