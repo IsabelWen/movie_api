38fcebea656cb50a2c8f6f61c0b4c4a2fff8b3e9
Isy <isy1997@freenet.de>
2023-11-07 21:34:49 +0100

    CREATE new favMovie for user with Mongoose in index.js

--- index.js
+++ index.js
@@ -119,20 +119,19 @@ app.put('/users/:id', (req, res) => {
 })
 
 // CREATE new favorite movie for user [UPDATED]
-app.post('/users/:id/:movieTitle', (req, res) => {
-    const id = req.params.id;
-    const movieTitle = req.params.movieTitle;
-
-    let user = users.find( user => user.id == id );
-
-
-    if (user) {
-        user.favoriteMovies.push(movieTitle);
-        res.status(200).send(movieTitle + ' has been added to user ' + id + '\'s array');
-    } else {
-        res.status(400).send('There is no such user')
-    }
-})
+app.post('/users/:Username/movies/:MovieID', async (req, res) => {
+    await Users.findOneAndUpdate({ Username: req.params.Username }, {
+        $push: { FavoriteMovies: req.params.MovieID }
+    },
+    { new: true }) // This line makes sure that the updated document is returned
+    .then((updatedUser) => {
+        res.json(updatedUser);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
 
 // DELETE favorite movie for user
 app.delete('/users/:id/:movieTitle', (req, res) => {
