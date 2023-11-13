42c7ed437fe2c443bbcace84b36055513e14b535
Isy <isy1997@freenet.de>
2023-11-13 11:28:07 +0100

    Add express validation to UPDATE user info endpoint

--- index.js
+++ index.js
@@ -124,12 +124,25 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false }), as
   Email: String, (required)
   Birthday: Date
 }*/
-app.put('/users/:Username', passport.authenticate('jwt', { session: false }), async (req, res) => {
+app.put('/users/:Username', passport.authenticate('jwt', { session: false }),
+    [
+        check('Username', 'Username is required').isLength({min: 5}),
+        check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+        check('Password', 'Password is required').not().isEmpty(),
+        check('Email', 'Email does not appear to be valid').isEmail()
+    ], async (req, res) => {
     // Condition to check user authorization
     if(req.user.Username !== req.params.Username){
         return res.status(400).send('Permission denied');
     }
     // Condition ends here
+
+    // check the validation object for errors
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+    
     let hashedPassword = Users.hashedPassword(req.body.Password);
     await Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
         {
