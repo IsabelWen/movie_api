546992f65987bb131a90dd4545e4e8c7d91ea93a
Isy <isy1997@freenet.de>
2023-11-13 18:15:44 +0100

    Update cors function all domains allowed

--- index.js
+++ index.js
@@ -8,19 +8,7 @@ const express = require('express'),
 const app = express();
 
 // CORS
-//app.use(cors()); --> will set the application to allow requests from all origins
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-app.use(cors({
-    origin: (origin, callback) => {
-        if(!origin) return callback(null, true);
-        if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-            return callback(new Error(message ), false);
-        }
-        return callback(null, true);
-    }
-}));
-
+app.use(cors()); //set the application to allow requests from all origins
 
 app.use(bodyParser.json()); //any time using req.body, the data will be expected to be in JSON format
 app.use(bodyParser.urlencoded({ extended: true }));
