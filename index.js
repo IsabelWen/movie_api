aa9fdcd2dc511614251cc5e13450d3405655ad06
Isy <isy1997@freenet.de>
2023-12-04 22:49:34 +0100

    Allow a specific set of origins to access your API CORS

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ const express = require('express'),
 const app = express();
 
 // CORS
-app.use(cors()); //set the application to allow requests from all origins
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'mymovie-api.netlify.app'];
+//allow specific set of origins to access your API
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            // If a specific origin isn’t found on the list of allowed origins
+            let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+            return callback(new Error(message), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 app.use(bodyParser.json()); //any time using req.body, the data will be expected to be in JSON format
 app.use(bodyParser.urlencoded({ extended: true }));
