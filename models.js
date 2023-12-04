4ba925cfa5f5918a075710961b52365c1522c8f3
Isy <isy1997@freenet.de>
2023-12-04 09:23:35 +0100

    Update date format birthday

--- models.js
+++ models.js
@@ -23,7 +23,7 @@ let userSchema = mongoose.Schema({
     Username: {type: String, required: true},
     Password: {type: String, required: true},
     Email: {type: String, require: true},
-    Birthdate: new Date("<YYYY-mm-dd>"),
+    Birthdate: {type: Date},
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
