37f9b6e9bf92627d3fb77d59c0dc628e7f0d21fa
Isy <isy1997@freenet.de>
2024-01-01 18:37:46 +0100

    Add useNewUrlParser and useUnifiedTopology to mongoose.connect

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 // mongoose.connect('mongodb://127.0.0.1:27017/movieDB', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect(process.env.CONNECTION_URI);
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 // CREATE new user
