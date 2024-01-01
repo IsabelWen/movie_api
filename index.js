0715bd4cdb9e72f09d95e97cfada45fc29d49b8c
Isy <isy1997@freenet.de>
2024-01-01 17:16:30 +0100

    remove useNewUrlParser from mongoose.connect

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 // mongoose.connect('mongodb://127.0.0.1:27017/movieDB', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.CONNECTION_URI);
 
 
 // CREATE new user
